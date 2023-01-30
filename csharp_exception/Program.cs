d228d8796989d5fe935fd60a957cd55c937d244c
LuoArkantos <luizfernandolima1997@gmail.com>
2023-01-30 13:29:11 -0300

    a

--- csharp_exception/Program.cs
+++ csharp_exception/Program.cs
@@ -2,15 +2,7 @@
 using csharp_exception.Contas;
 
 
-try
-{
-    ContaCorrente conta1 = new ContaCorrente(0, "1234-X");
-}
-catch(ArgumentException ex)
-{
-    Console.WriteLine("Parâmetro com erro" + ex.ParamName);
-    Console.WriteLine("Não é possível criar uma conta com o número de agência menor ou igual a zero!");
-    Console.WriteLine(ex.Message);
-    
-}
+ContaCorrente conta1 = new ContaCorrente(283, "1234-X");
+Console.WriteLine(ContaCorrente.TaxaOperacao);
+Console.ReadLine();
 
